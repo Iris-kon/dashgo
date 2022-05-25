299aa8ba822d4fc20a4ce5f80ad070b107b36e9d
Thiago Oliveira <thiagooliveira.k.on@gmail.com>
2022-05-25 03:53:08 -0300

    add responsiveness to user list

--- src/pages/users/index.tsx
+++ src/pages/users/index.tsx
@@ -1,10 +1,15 @@
-import { Box, Button, Checkbox, Flex, Heading, Icon, Table, Tbody, Td, Text, Th, Thead, Tr } from "@chakra-ui/react"
+import { Box, Button, Checkbox, Flex, Heading, Icon, Table, Tbody, Td, Text, Th, Thead, Tr, useBreakpointValue } from "@chakra-ui/react"
 import { RiAddLine, RiPencilLine } from "react-icons/ri"
 import { Header } from "../../components/Header"
 import { Pagination } from "../../components/Pagination"
 import { Sidebar } from "../../components/Sidebar"
 
 export default function UserList() {
+    const isWideVersion = useBreakpointValue({
+        base: false,
+        lg: true
+    })
+
     return (
         <Box>
             <Header />
@@ -31,11 +36,11 @@ export default function UserList() {
                     >
                         <Heading size="lg" fontWeight="normal">Usuários</Heading>
 
-                        <Button 
-                            as="a" 
-                            size="sm" 
-                            fontSize="small" 
-                            colorScheme="pink" 
+                        <Button
+                            as="a"
+                            size="sm"
+                            fontSize="small"
+                            colorScheme="pink"
                             leftIcon={<Icon as={RiAddLine} fontSize="20" />}
                         >
                             Criar novo
@@ -45,18 +50,18 @@ export default function UserList() {
                     <Table colorScheme="whiteAlpha">
                         <Thead>
                             <Tr>
-                                <Th px="6" color="gray.300" width="8">
+                                <Th px={["4","6"]} color="gray.300" width="8">
                                     <Checkbox colorScheme="pink" />
                                 </Th>
                                 <Th>Usuário</Th>
-                                <Th>Data de cadastro</Th>
+                                {isWideVersion && <Th>Data de cadastro</Th>}
                                 <Th width="8"></Th>
                             </Tr>
                         </Thead>
 
                         <Tbody>
                             <Tr>
-                                <Td px="6">
+                                <Td px={["4","6"]}>
                                     <Checkbox colorScheme="pink" />
                                 </Td>
                                 <Td>
@@ -65,18 +70,20 @@ export default function UserList() {
                                         <Text fontSize="small" color="gray.300">kouhi.my@gmail.com</Text>
                                     </Box>
                                 </Td>
-                                <Td>04 de junho, 2021</Td>
-                                <Td>
-                                <Button 
-                                    as="a" 
-                                    size="sm" 
-                                    fontSize="small" 
-                                    colorScheme="purple" 
-                                    leftIcon={<Icon as={RiPencilLine} fontSize="16" />}
-                                >
-                                    Editar
-                                </Button>
-                                </Td>
+                                {isWideVersion && <Td>04 de junho, 2021</Td>}
+                                {isWideVersion && (
+                                    <Td>
+                                        <Button
+                                            as="a"
+                                            size="sm"
+                                            fontSize="small"
+                                            colorScheme="purple"
+                                            leftIcon={<Icon as={RiPencilLine} fontSize="16" />}
+                                        >
+                                            Editar
+                                        </Button>
+                                    </Td>
+                                )}
                             </Tr>
                         </Tbody>
                     </Table>
