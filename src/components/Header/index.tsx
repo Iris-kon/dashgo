abf6a63636c2aea15533f8f304acb7f3a8f066b4
Thiago Oliveira <thiagooliveira.k.on@gmail.com>
2022-05-25 03:42:15 -0300

    responsive sidebar

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,10 +1,14 @@
-import { Flex, useBreakpointValue } from "@chakra-ui/react";
+import { Flex, Icon, IconButton, useBreakpointValue } from "@chakra-ui/react";
+import { RiMenuLine } from "react-icons/ri";
+import { useSidebarDrawer } from "../../Contexts/SidebarDrawerContext";
 import { Logo } from "./Logo";
 import { NotificationsNav } from "./NotificationsNav";
 import { Profile } from "./Profile";
 import { SearchBox } from "./SearchBox";
 
 export function Header() {
+    const { onOpen } = useSidebarDrawer()
+
     const isWideVersion = useBreakpointValue({
         base: false,
         lg: true
@@ -22,8 +26,20 @@ export function Header() {
             align="center"
 
         >
+            { !isWideVersion && (
+                <IconButton
+                    aria-label="Open Navigation"
+                    icon={<Icon as={RiMenuLine} />}
+                    fontSize="24"
+                    variant="unstyled"
+                    onClick={onOpen}
+                    mr="2"
+                >
+                </IconButton>
+            )}
+
             <Logo />    
-        
+
             {isWideVersion && <SearchBox />}
             
             <Flex
