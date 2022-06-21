6478e8f1f034ef1166e5735dc77283304d9d2223
Thiago Oliveira <thiagooliveira.k.on@gmail.com>
2022-06-21 05:10:25 -0300

    useUsers hook

--- src/pages/users/index.tsx
+++ src/pages/users/index.tsx
@@ -4,30 +4,10 @@ import { RiAddLine, RiPencilLine } from "react-icons/ri"
 import { Header } from "../../components/Header"
 import { Pagination } from "../../components/Pagination"
 import { Sidebar } from "../../components/Sidebar"
-import { useQuery } from "react-query"
+import { useUsers } from "../../services/hooks/useUsers"
 
 export default function UserList() {
-    const { data, isLoading, error } = useQuery('users', async () => {
-        const response = await fetch('http://localhost:3000/api/users')
-        const data = await response.json()
-
-        const users = data.users.map(user => {
-            return {
-                id: user.id,
-                name: user.name,
-                email: user.email,
-                createdAt: new Date(user.createdAt).toLocaleDateString('pt-BR', {
-                    day: '2-digit',
-                    month: 'long',
-                    year: 'numeric'
-                })
-            }
-        })
-
-        return users
-    }, {
-        staleTime: 1000 * 5 //5 seconds,
-    })
+    const { data, isLoading, isFetching, error } = useUsers()
 
     const isWideVersion = useBreakpointValue({
         base: false,
@@ -58,7 +38,11 @@ export default function UserList() {
                         justify="space-between"
                         align="center"
                     >
-                        <Heading size="lg" fontWeight="normal">Usuários</Heading>
+                        <Heading size="lg" fontWeight="normal">
+                            Usuários
+
+                            {!isLoading && isFetching && (<Spinner size="sm" color="gray.500" ml="4" />)}
+                        </Heading>
 
                         <Link href="/users/create" passHref>
                             <Button
