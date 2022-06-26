72537c642f94873c29ad1b868e9e7e9a9d2faf30
Thiago Oliveira <thiagooliveira.k.on@gmail.com>
2022-06-26 05:33:07 -0300

    pagination conclude new logic

--- src/components/Pagination/index.tsx
+++ src/components/Pagination/index.tsx
@@ -50,7 +50,7 @@ export function Pagination({
             >
                 {currentPage > (1 + sibligsCount) && (
                     <>
-                        <PaginationItem number={1} />
+                        <PaginationItem onPageChange={onPageChange} number={1} />
                         { currentPage > (2 + sibligsCount) && (
                             <Text 
                                 color="gray.300"
@@ -62,13 +62,13 @@ export function Pagination({
                 )}
                 
                 {previousPages.length > 0 && previousPages.map(page => {
-                    return <PaginationItem key={page} number={page} />
+                    return <PaginationItem onPageChange={onPageChange} key={page} number={page} />
                 })}
 
-                <PaginationItem number={currentPage} isCurrent />
+                <PaginationItem onPageChange={onPageChange} number={currentPage} isCurrent />
         
                 {nextPages.length > 0 && nextPages.map(page => {
-                    return <PaginationItem key={page} number={page} />
+                    return <PaginationItem onPageChange={onPageChange} key={page} number={page} />
                 })}
 
                 {(1 + sibligsCount) < lastPage && (
@@ -80,7 +80,7 @@ export function Pagination({
                                 textAlign="center"
                             >...</Text>
                         )}
-                        <PaginationItem number={lastPage} />
+                        <PaginationItem onPageChange={onPageChange} number={lastPage} />
                     </>
                 )}
 
